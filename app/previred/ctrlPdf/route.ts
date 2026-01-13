dcc037260f2c2abcfd2214ded96e6cf63bd56ece
Marcos <socram174@hotmail.com>
2026-01-13 12:29:08 -0300

    qdw

--- app/previred/ctrlPdf/route.ts
+++ app/previred/ctrlPdf/route.ts
@@ -3,18 +3,19 @@ import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
+// Add this to prevent static generation
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
     console.log(request)
   try {
-    // Get the PDF file from the public folder
     const pdfPath = path.join(process.cwd(), 'public', 'pdfs', 'sample1.pdf');
     const pdfBuffer = fs.readFileSync(pdfPath);
 
-    // Return the PDF with appropriate headers
     return new NextResponse(pdfBuffer, {
       headers: {
         'Content-Type': 'application/pdf',
-        'Content-Disposition': 'inline; filename="document.pdf"', // 'inline' shows in browser, 'attachment' downloads
+        'Content-Disposition': 'inline; filename="document.pdf"',
       },
     });
   } catch (error) {
